bb9b25325a74e092479abf74c24180975af568b3
songdaochuanshu <songdaochuanshu@gmail.com>
2022-03-30 18:17:34 +0800

    🔧 修改线上url

--- src/https/request/config.ts
+++ src/https/request/config.ts
@@ -2,6 +2,6 @@
 export default class Base {
     static BASE_URL = process.env.NODE_ENV === "development"
         ? "https://mock.mengxuegu.com/mock/606f08eeb43557571f7269a4"
-        : "https://produceCommon.com(生产线地址)"
+        : "https://mock.mengxuegu.com/mock/606f08eeb43557571f7269a4"
     static TIME_OUT = 10000;
 }
